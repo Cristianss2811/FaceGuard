ff180305a8189f86a9df2258241d619cb707761c
Cristianss2811 <cristianss.2811@gmail.com>
2024-10-26 19:16:31 -0600

    Agregar sidebar al base.html

---
+++ tasks/static/js/scripts.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPath = window.location.pathname;
+    const links = document.querySelectorAll('.sidebar-link');
+
+    function setActiveLink(activePage) {
+        links.forEach(link => {
+            if (link.getAttribute('href') === activePage || 
+                (link.getAttribute('href').startsWith('#') && activePage.includes(link.getAttribute('data-page')))) {
+                link.classList.add('active');
+                localStorage.setItem('activePage', activePage);
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+
+    // Verificar si hay un estado activo guardado
+    const savedActivePage = localStorage.getItem('activePage');
+    if (savedActivePage) {
+        setActiveLink(savedActivePage);
+    } else {
+        // Si no hay estado guardado, establecer Dashboard como activo por defecto
+        setActiveLink('/'); // Cambia esto si el href de Dashboard es diferente
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            setActiveLink(link.getAttribute('href'));
+        });
+    });
+});
+
+function logout() {
+    localStorage.removeItem('activePage');
+}
+
